83710d632e618a68d19efe605409b35517d42ee2
github username <github email address>
2020-01-14 15:55:27 -0500

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,26 @@
 class Car
+  attr_accessor :make, :model, :classification, :owner, :mechanic
+  @@all = []
 
-  attr_reader :make, :model
-
-  def initialize(make, model, classification)
+  def initialize(make, model, classification, owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @mechanic = mechanic
+    @@all << self
+  end
+
+  def self.classifications
+    all.map(&:classification)
+  end
+  
+  def mechanics_specialization
+    Mechanic.all.select { |mechanic| mechanic.specialty == self.classification }
+  end
+
+  def self.all
+    @@all
   end
 
 end
--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,29 @@
 class CarOwner
-
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def cars #<-- helper method to connect w/ connector
+    Car.all.select { |car| car.owner == self }
+  end
+
+  def owners_mechanic
+    cars.map(&:mechanic))
+  end
+
+
+  def self.avg_cars_owned
+    sum_cars = Car.all.length
+    sum_owners = all.length
+    avg = (sum_cars / sum_owners).to_f
+  end
+
+  def self.all
+    @@all
+  end
 
 end
