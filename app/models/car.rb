28083624e5e3008d6153de92d4285f812249b9f3
github username <github email address>
2020-01-14 09:58:58 -0500

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,34 @@
+ # join class
+
 class Car
 
-  attr_reader :make, :model
-
-  def initialize(make, model, classification)
+  attr_reader :owner, :mechanic, :classification, :make, :model
+  @@all = []
+ 
+  def initialize(owner, mechanic, classification, make, model)
+    @owner = owner
+    @mechanic = mechanic
+    @classification = classification
     @make = make
     @model = model
-    @classification = classification
+    @@all << self
   end
+
+  def self.all 
+    @@all
+  end 
+
+  def self.classifications
+    self.all.map do |instance|
+      instance.classification
+    end.uniq
+  end 
+
+  def find_mechanic
+    Mechanic.all.select do |instance|
+      instance.specialty == self.classification
+    end 
+    # want to find something that  == self.classification
+  end 
 
 end
--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,33 @@
 class CarOwner
 
   attr_reader :name
+  @@all = []
+  
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all 
+    @@all
+  end 
+
+  def cars
+    Car.all.select do |car_instance|
+      car_instance.owner == self 
+    end 
+  end
+
+  def mechanics 
+    #self.cars.mechanic  # first try # got it after thinking on the toilet!
+    self.cars.map do |car_instance|
+      car_instance.mechanic
+    end 
+  end
+
+  def self.average # this is a class method?
+    Car.all.length/self.all.length
+  end 
 
 end
